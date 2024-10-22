40ec46bd68af4318ab4502d8c1055069ec2dc121
Luis Felipe <llipeandrade34@gmail.com>
2024-10-21 22:20:47 -0300

    feat: adicionando conexão ao banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import app from "./app";
 import { env } from "./env";
+import connect from "./db/connection";
 
 
 app.listen(env.PORT, () => {
+    connect();
     console.log(`Server is running on port ${env.PORT}`);
 });
